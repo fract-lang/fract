ab8c82159c3973a63b69ce29d0864ce02fad00fc
Mertcan Davulcu <54983926+mertcandav@users.noreply.github.com>
2021-01-22 20:16:14 +0300

    Add signed_byte.hh

---
+++ Objects/signed_byte.hh
@@ -0,0 +1,8 @@
+#ifndef __SIGNED_BYTE_HH
+#define __SIGNED_BYTE_HH
+
+#define signed_byte_default "0"
+#define signed_byte_minimum "-128"
+#define signed_byte_maximum "127"
+
+#endif // __SIGNED_BYTE_HH
