4ea51fe1cf8a5ac608ec2b5bf5091f04d2a3c0d0
Mertcan Davulcu <54983926+mertcandav@users.noreply.github.com>
2021-01-22 20:19:49 +0300

    Add short.hh

---
+++ Objects/unsigned_short.hh
@@ -0,0 +1,8 @@
+#ifndef __UNSIGNED_SHORT_HH
+#define __UNSIGNED_SHORT_HH
+
+#define unsigned_short_default "0"
+#define unsigned_short_minimum "0"
+#define unsigned_short_maximum "65535"
+
+#endif // __UNSIGNED_SHORT_HH
