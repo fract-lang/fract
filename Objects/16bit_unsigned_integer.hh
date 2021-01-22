0dbfd298640d100308ebfd5cfec856ab85b6647d
Mertcan Davulcu <54983926+mertcandav@users.noreply.github.com>
2021-01-22 20:31:05 +0300

    Improvements on types

---
+++ Objects/16bit_unsigned_integer.hh
@@ -0,0 +1,8 @@
+#ifndef __16BIT_UNSIGNED_INTEGER_HH
+#define __16BIT_UNSIGNED_INTEGER_HH
+
+#define _16bit_unsigned_integer_default "0"
+#define _16bit_unsigned_integer_minimum "0"
+#define _16bit_unsigned_integer_maximum "65535"
+
+#endif // __16BIT_UNSIGNED_INTEGER_HH
--- Objects/short.hh
+++
@@ -1,8 +0,0 @@
-#ifndef __SHORT_HH
-#define __SHORT_HH
-
-#define short_default "0"
-#define short_minimum "-32768"
-#define short_maximum "32767"
-
-#endif // __SHORT_HH
