c6b4eb984b840ffa043c9f4a36ca132bb0fa1ffe
Mertcan Davulcu <54983926+mertcandav@users.noreply.github.com>
2021-01-23 09:42:31 +0300

    Add 32bit_integer.hh

---
+++ Objects/32bit_integer.hh
@@ -0,0 +1,8 @@
+#ifndef __32BIT_INTEGER_HH
+#define __32BIT_INTEGER_HH
+
+#define _32bit_integer_default "0"
+#define _32bit_integer_minimum "-2147483648"
+#define _32bit_integer_maximum "2147483647"
+
+#endif // __32BIT_INTEGER_HH
