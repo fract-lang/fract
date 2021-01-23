f8bb39b42312275d6b6f6dd448f55e6ab38bdc2d
Mertcan Davulcu <54983926+mertcandav@users.noreply.github.com>
2021-01-23 09:45:28 +0300

    Add 64bit_integer.hh

---
+++ Objects/64bit_integer.hh
@@ -0,0 +1,8 @@
+#ifndef __64BIT_INTEGER_HH
+#define __64BIT_INTEGER_HH
+
+#define _64bit_integer_default "0"
+#define _64bit_integer_minimum "-9223372036854775808"
+#define _64bit_integer_maximum "9223372036854775807"
+
+#endif // __64BIT_INTEGER_HH
