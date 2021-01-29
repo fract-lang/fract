a022bd4c75f39334aeb4abb804c72d3d4d757d18
mertcandav <mertcandav@outlook.com>
2021-01-29 13:54:45 +0300

    Add make.go

--- shell/modules/version/version.go
+++ shell/modules/version/version.go
@@ -3,6 +3,7 @@ package version
 import "fmt"
 
 // Process Process command in module.
+// command Command to process.
 func Process(command string) {
 	if command != "" {
 		fmt.Println("This module can only be used!")
