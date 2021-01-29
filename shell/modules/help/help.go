2be49502a37c0df7d74943297f9f3b903a407436
mertcandav <mertcandav@outlook.com>
2021-01-29 13:11:47 +0300

    Add help.go

---
+++ shell/modules/help/help.go
@@ -0,0 +1,21 @@
+package help
+
+import (
+	"fmt"
+
+	"../../utilities/cli"
+)
+
+// Process Process command in module.
+func Process(command string) {
+	if command != "" {
+		fmt.Println("This module can only be used!")
+		return
+	}
+	cli.PrintMapAsTable(map[string]string{
+		"make":    "Interprete Fract code.",
+		"version": "Show version.",
+		"help":    "Show help.",
+		"exit":    "Exit.",
+	})
+}
