9465183f5473150a485f58f7c7e16e9456ae2ae2
mertcandav <mertcand450@hotmail.com>
2021-01-29 17:29:06 +0300

    Add parser/types.go

---
+++ parser/types.go
@@ -0,0 +1,14 @@
+package parser
+
+const (
+	//PTypeNone NA
+	PTypeNone int = -1
+	//PTypeAddition Addition.
+	PTypeAddition int = 0
+	//PTypeMultiplication Multiplication.
+	PTypeMultiplication int = 1
+	//PTypeDivision Division.
+	PTypeDivision int = 2
+	//PTypeSubtraction Subtraction.
+	PTypeSubtraction int = 3
+)
