08c70c1018e8d12764e9fd10ff67dbf10d0c89f0
mertcandav <mertcandav@outlook.com>
2021-01-29 11:04:17 +0300

    Removed files

--- Include/fract.hh
+++
@@ -1,9 +0,0 @@
-#ifndef __FRACT_HH
-#define __FRACT_HH
-
-/// @brief Version of Fract.
-#define fract_version "0.0.1"
-/// @brief File extension of Fract.
-#define fract_extension ".fract"
-
-#endif // __FRACT_HH
