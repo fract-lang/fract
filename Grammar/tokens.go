3a3e9fb68375e6f92cc3f5f38c16e8b3fd13fb09
mertcandav <mertcandav@outlook.com>
2021-01-29 12:04:12 +0300

    Add tokens.go

---
+++ Grammar/tokens.go
@@ -0,0 +1,141 @@
+package grammar
+
+// GENERIC TOKENS
+
+// TokenSharp Sharp.
+const TokenSharp string = "#"
+
+// TokenPlus Plus.
+const TokenPlus string = "+"
+
+// TokenMinus Minus.
+const TokenMinus string = "-"
+
+// TokenStar Star.
+const TokenStar string = "*"
+
+// TokenPercent Percent.
+const TokenPercent string = "%"
+
+// TokenSlash Slash.
+const TokenSlash string = "/"
+
+// TokenReverseSlash Rever slash.
+const TokenReverseSlash string = "\\"
+
+// TokenEquals Equals.
+const TokenEquals string = "="
+
+// TokenQuestion Question mark.
+const TokenQuestion string = "?"
+
+// TokenVerticalBar Vertical bar.
+const TokenVerticalBar string = "|"
+
+// TokenGreat Greater then.
+const TokenGreat string = ">"
+
+// TokenLess Less then.
+const TokenLess string = "<"
+
+// TokenSemicolon Semicolon.
+const TokenSemicolon string = ";"
+
+// TokenColon Colon.
+const TokenColon string = ":"
+
+// TokenComma Comma.
+const TokenComma string = ","
+
+// TokenExclamation Exclamation.
+const TokenExclamation string = "!"
+
+// TokenAmper Amper.
+const TokenAmper string = "&"
+
+// TokenAt At.
+const TokenAt string = "@"
+
+// TokenDot Dot.
+const TokenDot string = "."
+
+// TokenLParenthes Left parentheses.
+const TokenLParenthes string = "("
+
+// TokenRParenthes Right parentheses.
+const TokenRParenthes string = ")"
+
+// MULTICHAR OPERATORS
+
+// SeperatorSub Sub element seperator.
+const SeperatorSub string = TokenColon + TokenColon
+
+// KEYWORDS
+
+// KwImport Import packages.
+const KwImport string = "use"
+
+// KwFunction Function define.
+const KwFunction string = "fn"
+
+// KwDelete Delete variable.
+const KwDelete string = "del"
+
+// KwVariable Variable define.
+const KwVariable string = "var"
+
+// KwBlockFinal Block terminator.
+const KwBlockFinal string = "end"
+
+// KwReturn Returns.
+const KwReturn string = "ret"
+
+// KwForLoop For loop.
+const KwForLoop string = "for"
+
+// KwWhileLoop While loop.
+const KwWhileLoop string = "while"
+
+// KwIf If condition.
+const KwIf string = "if"
+
+// KwElseIf Else-If alternate.
+const KwElseIf string = "elif"
+
+// KwElse Else.
+const KwElse string = "else"
+
+// DATA TYPES
+
+// DtByte byte.
+const DtByte string = "byte"
+
+// DtSignedByte sbyte.
+const DtSignedByte string = "sbyte"
+
+// Dt16BitInteger short.
+const Dt16BitInteger string = "int16"
+
+// Dt32BitInteger int.
+const Dt32BitInteger string = "int32"
+
+// Dt64BitInteger long.
+const Dt64BitInteger string = "int64"
+
+// DtUnsigned16BitInteger ushort.
+const DtUnsigned16BitInteger string = "int16"
+
+// DtUnsigned32BitInteger uint.
+const DtUnsigned32BitInteger string = "int32"
+
+// DtUnsigned64BitInteger ulong.
+const DtUnsigned64BitInteger string = "int64"
+
+// DtBoolean boolean.
+const DtBoolean string = "bool"
+
+// DtFloat float.
+const DtFloat string = "float"
+
+// DtDouble double.
+const DtDouble string = "double"
