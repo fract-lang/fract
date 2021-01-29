aec4d8e729bbdf235385496dd165dc4787869b84
mertcandav <mertcand450@hotmail.com>
2021-01-29 12:05:48 +0300

    Update cli package

--- utilities/cli/cli.go
+++ utilities/cli/cli.go
@@ -6,6 +6,32 @@ import (
 	"os"
 )
 
+// Returns string whitespace by count.
+// count Count of whitespace.
+func getws(count int) string {
+	var str string = ""
+	for counter := 1; counter <= count; counter++ {
+		str += " "
+	}
+	return str
+}
+
+// PrintMapAsTable Print map to cli screen as table.
+// dict Map to print.
+func PrintMapAsTable(dict map[string]string) {
+	var maxlen int = 0
+	for key := range dict {
+		if maxlen < len(key) {
+			maxlen = len(key)
+		}
+	}
+	maxlen += 5
+	for key := range dict {
+		fmt.Println(key + " " + getws(maxlen-len(key)) +
+			dict[key])
+	}
+}
+
 // Input Returns input from command-line.
 // message Input message.
 func Input(message string) string {
