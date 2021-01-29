b8ed88bfcd0387c85a2a1bbd2749979972311349
mertcandav <mertcandav@outlook.com>
2021-01-29 16:45:33 +0300

    Add OpenFile function to fs.go

--- utilities/fs/fs.go
+++ utilities/fs/fs.go
@@ -64,6 +64,16 @@ func WriteText(path string, content string) {
 	file.Close()
 }
 
+// OpenFile Open file by path.
+// path Path to open.
+func OpenFile(path string) *os.File {
+	file, err := os.OpenFile(path, os.O_RDWR, 0644)
+	if err != nil {
+		return file
+	}
+	return nil
+}
+
 // WriteAllText Remove contetn and write this content.
 // path Destination path.
 // content Content to write.
