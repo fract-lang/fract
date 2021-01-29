6a30331c2f1f2010893c0e8154412a697c197e9e
mertcandav <mertcand450@hotmail.com>
2021-01-29 11:12:48 +0300

    Add variable.go

---
+++ objects/variable.go
@@ -0,0 +1,11 @@
+package objects
+
+// Variable Variable instance.
+type Variable struct {
+	// Name of variable.
+	name string
+	// Value of variable.
+	value string
+	// Type of variable.
+	group int
+}
