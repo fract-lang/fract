bdb65f95d9d6d2c224ed70f61b1f7bba4b94fdb1
mertcandav <mertcandav@outlook.com>
2021-01-29 12:57:17 +0300

    Add command_processor.go

---
+++ shell/commands/command_processor.go
@@ -0,0 +1,45 @@
+package commands
+
+import (
+	"container/list"
+	"regexp"
+	"strings"
+)
+
+// GetNamespace Get namespace of command.
+// command Command.
+func GetNamespace(command string) string {
+	var position int = strings.Index(command, " ")
+	if position == -1 {
+		return command
+	}
+	return command[0:position]
+}
+
+// RemoveNamespace Remove namespace from command.
+// command Command.
+func RemoveNamespace(command string) string {
+	var position int = strings.Index(command, " ")
+	if position == -1 {
+		return ""
+	}
+	return command[position:]
+}
+
+// GetArguments Get arguments of command.
+// command Command.
+func GetArguments(command string) list.List {
+	args := list.New()
+	pattern := regexp.MustCompile("(^|\\s+)-\\w+(?=($|\\s+))")
+	for arg := range pattern.FindAllString(command, -1) {
+		args.PushBack(arg)
+	}
+	return args
+}
+
+// RemoveArguments Remove arguments from command.
+// command Command.
+func RemoveArguments(command string) string {
+	pattern := regexp.MustCompile("(^|\\s+)-\\w+(?=($|\\s+))")
+	return pattern.ReplaceAllString(command, "")
+}
