0054be04bfb0ed0ff80e3ea7d3f95bd21d5d3d6b
mertcandav <mertcandav@outlook.com>
2021-01-31 21:33:23 +0300

    Fix arithmetic pattern

--- src/lexer/lexer.go
+++ src/lexer/lexer.go
@@ -80,10 +80,10 @@ func (l *Lexer) Generate() objects.Token {
 
 	/* Tokenize. */
 	arithmeticCheck := strings.TrimSpace(regexp.MustCompile(
-		"^(-|)\\s*[0-9]+(\\.[0-9]+)?(\\s+||\\W+|$)").FindString(ln))
+		"^(-|)\\s*[0-9]+(\\.[0-9]+)?(\\s+||\\W|$)").FindString(ln))
 	if arithmeticCheck != "" &&
 		(lastToken.Value == "" || lastToken.Type == fract.TypeOperator) { // Numeric value.
-		match, _ := regexp.MatchString("\\W+$", arithmeticCheck)
+		match, _ := regexp.MatchString("\\W$", arithmeticCheck)
 		if match {
 			arithmeticCheck = arithmeticCheck[:len(arithmeticCheck)-1]
 		}
