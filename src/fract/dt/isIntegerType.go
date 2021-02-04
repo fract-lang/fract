2c279cd10fa9bed836e2bb82a2e263b0c6509cf9
mertcandav <mertcandav@outlook.com>
2021-02-04 20:10:08 +0300

    Fix 8bit integer support on type system

--- src/fract/dt/isIntegerType.go
+++ src/fract/dt/isIntegerType.go
@@ -10,9 +10,11 @@ import "../../grammar"
 // _type Type to check.
 func IsIntegerType(_type string) bool {
 	return _type == grammar.DtInt8 ||
+		_type == grammar.DtInt16 ||
 		_type == grammar.DtInt32 ||
 		_type == grammar.DtInt64 ||
 		_type == grammar.DtUInt8 ||
+		_type == grammar.DtUInt16 ||
 		_type == grammar.DtUInt32 ||
 		_type == grammar.DtUInt64
 }
