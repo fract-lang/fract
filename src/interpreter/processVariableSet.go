6d79d88bd07d2e71da340ab31b6896c894411249
mertcandav <mertcandav@outlook.com>
2021-02-04 18:21:44 +0300

    Add defined variable value set support

---
+++ src/interpreter/processVariableSet.go
@@ -0,0 +1,48 @@
+/*
+	processVariableSet Function.
+*/
+
+package interpreter
+
+import (
+	"../fract"
+	"../fract/dt"
+	"../fract/name"
+	"../grammar"
+	"../objects"
+	"../utilities/vector"
+)
+
+// ProcessVariableSet Process variable set statement.
+// tokens Tokens to process.
+func (i *Interpreter) processVariableSet(tokens *vector.Vector) {
+	/* Check size. */
+	if tokens.Len() < 3 {
+		fract.Error(tokens.Last().(objects.Token), "")
+	}
+
+	_name := tokens.At(0).(objects.Token)
+
+	index := name.VarIndexByName(i.vars, _name.Value)
+	if index == -1 {
+		fract.Error(_name, "Name is not defined!: "+_name.Value)
+	}
+
+	setter := tokens.At(1).(objects.Token)
+	// Setter is not a setter operator?
+	if setter.Type != fract.TypeOperator && setter.Value != grammar.Setter {
+		fract.Error(setter, "This is not a setter operator!"+setter.Value)
+	}
+
+	variable := i.vars.At(index).(objects.Variable)
+	valtokens := tokens.Sublist(2, tokens.Len()-2)
+	value := i.processValue(&valtokens)
+
+	// Check value and data type compatibility.
+	if dt.IsIntegerType(variable.Type) && value.Type != fract.VTInteger {
+		fract.Error(setter, "Value and data type is not compatible!")
+	}
+
+	variable.Value = value.Content
+	i.vars.Set(index, variable)
+}
