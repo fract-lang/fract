2cd421277d1f886841c0f7fdce79f9692c51768f
mertcandav <mertcand450@hotmail.com>
2021-02-06 11:13:57 +0300

    Added notequals operator

--- src/interpreter/findConditionOperator.go
+++ src/interpreter/findConditionOperator.go
@@ -12,7 +12,8 @@ import (
 func findConditionOperator(tokens *vector.Vector) (int, string) {
 	for index := 0; index < tokens.Len(); index++ {
 		current := tokens.At(index).(objects.Token)
-		if current.Type == fract.TypeOperator && (current.Value == grammar.TokenEquals) {
+		if current.Type == fract.TypeOperator && (current.Value == grammar.TokenEquals ||
+			current.Value == grammar.NotEquals) {
 			return index, current.Value
 		}
 	}
