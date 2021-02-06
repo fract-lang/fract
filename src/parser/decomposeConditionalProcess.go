3afea61344476cdb86e9904631e5fed968d4ed58
mertcandav <mertcandav@outlook.com>
2021-02-06 12:07:12 +0300

    Add generic compatibility to decomposeOrConditionalProcess function of parser package and rename to decomposeConditionalProcess.

--- src/parser/decomposeConditionalProcess.go
+++ src/parser/decomposeConditionalProcess.go
@@ -1,12 +1,11 @@
 /*
-	DecomposeOrConditionalProcess Function.
+	DecomposeConditionalProcess Function.
 */
 
 package parser
 
 import (
 	"../fract"
-	"../grammar"
 	"../objects"
 	"../utilities/vector"
 )
@@ -14,30 +13,32 @@ import (
 // findNextOrOperator Find next or condition operator index and return if find, return -1 if not.
 // tokens Tokens to find.
 // pos Position of start to find.
-func findNextOrOperator(tokens *vector.Vector, pos int) int {
+// operator Operator to find.
+func findNextOperator(tokens *vector.Vector, pos int, operator string) int {
 	for ; pos < tokens.Len(); pos++ {
 		current := tokens.At(pos).(objects.Token)
-		if current.Type == fract.TypeOperator && current.Value == grammar.TokenVerticalBar {
+		if current.Type == fract.TypeOperator && current.Value == operator {
 			return pos
 		}
 	}
 	return -1
 }
 
-// DecomposeOrConditionalProcess Decompose and returns conditional expressions by operators.
+// DecomposeConditionalProcess Decompose and returns conditional expressions by operators.
 // tokens Tokens to process.
-func DecomposeOrConditionalProcess(tokens *vector.Vector) vector.Vector {
+// operator Operator to decompose.
+func DecomposeConditionalProcess(tokens *vector.Vector, operator string) vector.Vector {
 	expressions := *vector.New()
 
 	last := 0
-	index := findNextOrOperator(tokens, last)
+	index := findNextOperator(tokens, last, operator)
 	if index == 0 { // Operator is first element of vector?
 		fract.Error(tokens.First().(objects.Token), "Operator spam!")
 	}
 	for index != -1 {
 		expressions.Append(tokens.Sublist(last, index-last))
 		last = index + 1
-		index = findNextOrOperator(tokens, last) // Find next.
+		index = findNextOperator(tokens, last, operator) // Find next.
 		if index == tokens.Len()-1 {
 			fract.Error(tokens.Last().(objects.Token), "Operator defined, but for what?")
 		}
