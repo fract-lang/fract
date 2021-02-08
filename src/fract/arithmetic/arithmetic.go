ac73dbe120e7a7bfd78086e482ca1b23672eb8d6
mertcandav <mertcandav@outlook.com>
2021-02-08 12:29:43 +0300

    Remove unused functions from arithmetic package

--- src/fract/arithmetic/arithmetic.go
+++ src/fract/arithmetic/arithmetic.go
@@ -11,27 +11,6 @@ import (
 	"../../grammar"
 )
 
-// IsNegative Is negative number?
-// value Value to check.
-func IsNegative(value string) bool {
-	return strings.HasPrefix(value, grammar.TokenMinus)
-}
-
-// IsNumeric Char is numeric?
-// char Char to check.
-func IsNumeric(char byte) bool {
-	return char == '0' ||
-		char == '1' ||
-		char == '2' ||
-		char == '3' ||
-		char == '4' ||
-		char == '5' ||
-		char == '6' ||
-		char == '7' ||
-		char == '8' ||
-		char == '9'
-}
-
 // TypeToString Parse type to string.
 // _type Type.
 // value Value to parse.
