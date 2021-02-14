699e32e39bbed1f7ab6831a9c023760f9fc63081
mertcandav <mertcandav@outlook.com>
2021-02-14 19:54:00 +0300

    Split functions as file of fract package

--- src/fract/error.go
+++ src/fract/error.go
@@ -1,13 +1,10 @@
 /*
-	ERROR FUNCTIONS
+	Error Function.
 */
 
 package fract
 
 import (
-	"fmt"
-	"os"
-
 	"../objects"
 )
 
@@ -16,15 +13,4 @@ import (
 // message Message of error.
 func Error(token objects.Token, message string) {
 	ErrorCustom(token.File.Path, token.Line, token.Column, message)
-}
-
-// ErrorCustom Exit with error.
-// path File path of error.
-// line Line of error.
-// column Column of error.
-// message Message of error.
-func ErrorCustom(path string, line int, column int, message string) {
-	fmt.Printf("RUNTIME ERROR\nFILE: %s\nMessage: %s\nLINE: %d\nCOLUMN: %d",
-		path, message, line, column)
-	os.Exit(1)
 }
