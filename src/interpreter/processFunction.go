9a2705c485f9db873828b20040eed192d7f52591
mertcandav <mertcandav@outlook.com>
2021-03-07 13:37:26 +0300

    Update function execute option

--- src/interpreter/processFunction.go
+++ src/interpreter/processFunction.go
@@ -76,7 +76,8 @@ func (i *Interpreter) processFunction(tokens *vector.Vector) {
 		}
 	}
 
+	i.skipBlock()
+	function.Tokens = i.tokens.Sublist(function.Start, i.index-function.Start)
 	i.funcs.Vals = append(i.funcs.Vals, function)
 
-	i.skipBlock()
 }
