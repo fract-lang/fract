c0b58cf4a68a68d265a1d5b2e663f459322c85ad
mertcandav <mertcand450@hotmail.com>
2021-03-12 23:27:00 +0300

    Add function support to delete keyword

--- src/interpreter/processDelete.go
+++ src/interpreter/processDelete.go
@@ -6,6 +6,7 @@ package interpreter
 
 import (
 	"../fract"
+	"../grammar"
 	"../objects"
 	"../utilities/vector"
 )
@@ -13,15 +14,17 @@ import (
 // processDelete Process delete keyword.
 // tokens Tokens to process.
 func (i *Interpreter) processDelete(tokens vector.Vector) {
+	tokenLen := len(tokens.Vals)
+
 	// Value is not defined?
-	if len(tokens.Vals) < 2 {
+	if tokenLen < 2 {
 		first := tokens.Vals[0].(objects.Token)
 		fract.ErrorCustom(first.File.Path, first.Line, first.Column+len(first.Value),
 			"Value is not found!")
 	}
 
 	comma := false
-	for index := 1; index < len(tokens.Vals); index++ {
+	for index := 1; index < tokenLen; index++ {
 		current := tokens.Vals[index].(objects.Token)
 
 		if comma {
@@ -35,6 +38,28 @@ func (i *Interpreter) processDelete(tokens vector.Vector) {
 		// Token is not a deletable object?
 		if current.Type != fract.TypeName {
 			fract.Error(current, "This is not deletable object!")
+		}
+
+		if index < tokenLen-1 {
+			next := tokens.Vals[index+1].(objects.Token)
+			if next.Type == fract.TypeBrace && next.Value == grammar.TokenLParenthes {
+				nnext := tokens.Vals[index+2].(objects.Token)
+				if !(nnext.Type == fract.TypeBrace && nnext.Value == grammar.TokenRParenthes) {
+					fract.Error(nnext, "Invalid syntax!")
+				}
+				index += 2
+
+				position := i.functionIndexByName(current.Value)
+
+				// Name is not defined?
+				if position == -1 {
+					fract.Error(current, "Name is not defined!")
+				}
+
+				i.funcs.RemoveRange(position, 1)
+				comma = true
+				continue
+			}
 		}
 
 		position := i.varIndexByName(current.Value)
