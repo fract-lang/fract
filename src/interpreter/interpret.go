2522f082a64a5c4728ff4710ec7d19f943c9f76e
mertcandav <mertcandav@outlook.com>
2021-03-16 18:05:13 +0300

    -

--- src/interpreter/interpret.go
+++ src/interpreter/interpret.go
@@ -30,24 +30,26 @@ func (i *Interpreter) Interpret() {
 	}
 
 	// Change blocks.
-	count := 0
-	last := -1
-	for i.index = range i.tokens.Vals {
-		first := i.tokens.Vals[i.index].(vector.Vector).Vals[0].(objects.Token)
-		if first.Type == fract.TypeBlockEnd {
-			count--
-		} else if first.Type == fract.TypeIf || first.Type == fract.TypeLoop ||
-			first.Type == fract.TypeFunction {
-			count++
-			if count == 1 {
-				last = i.index
+	{
+		count := 0
+		last := -1
+		for i.index = range i.tokens.Vals {
+			first := i.tokens.Vals[i.index].(vector.Vector).Vals[0].(objects.Token)
+			if first.Type == fract.TypeBlockEnd {
+				count--
+			} else if first.Type == fract.TypeIf || first.Type == fract.TypeLoop ||
+				first.Type == fract.TypeFunction {
+				count++
+				if count == 1 {
+					last = i.index
+				}
 			}
 		}
-	}
 
-	if count > 0 { // Check blocks.
-		fract.Error(i.tokens.Vals[last].(vector.Vector).Vals[0].(objects.Token),
-			"Block is expected ending...")
+		if count > 0 { // Check blocks.
+			fract.Error(i.tokens.Vals[last].(vector.Vector).Vals[0].(objects.Token),
+				"Block is expected ending...")
+		}
 	}
 
 	// Interpret all lines.
