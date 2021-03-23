90945eb4be42a004e79b045c44721ea7d6d7c05f
mertcandav <mertcandav@outlook.com>
2021-03-23 14:24:12 +0300

    Fix value parsing of input operator

--- internal/interpreter/processInput.go
+++ internal/interpreter/processInput.go
@@ -5,7 +5,6 @@
 package interpreter
 
 import (
-	"github.com/fract-lang/fract/pkg/arithmetic"
 	"github.com/fract-lang/fract/pkg/cli"
 	"github.com/fract-lang/fract/pkg/fract"
 	obj "github.com/fract-lang/fract/pkg/objects"
@@ -15,14 +14,8 @@ import (
 // tokens Tokens to process.
 func (i *Interpreter) processInput(tokens []obj.Token) obj.Value {
 	printValue(i.processValue(&tokens))
-	input := cli.Input("")
-	value := obj.Value{
-		Content: []string{},
+	return obj.Value{
+		Content: []string{cli.Input("")},
 		Type:    fract.VALString,
-		Array:   true,
 	}
-	for _, char := range input {
-		value.Content = append(value.Content, arithmetic.IntToString(char))
-	}
-	return value
 }
