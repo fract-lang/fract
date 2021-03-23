a7a9aa8980535538e9bc1ad2d3766cdafdca350f
mertcandav <mertcandav@outlook.com>
2021-03-23 14:21:10 +0300

    Use cli on input function

--- internal/interpreter/processInput.go
+++ internal/interpreter/processInput.go
@@ -5,10 +5,8 @@
 package interpreter
 
 import (
-	"bufio"
-	"os"
-
 	"github.com/fract-lang/fract/pkg/arithmetic"
+	"github.com/fract-lang/fract/pkg/cli"
 	"github.com/fract-lang/fract/pkg/fract"
 	obj "github.com/fract-lang/fract/pkg/objects"
 )
@@ -17,9 +15,7 @@ import (
 // tokens Tokens to process.
 func (i *Interpreter) processInput(tokens []obj.Token) obj.Value {
 	printValue(i.processValue(&tokens))
-	scanner := bufio.NewScanner(os.Stdin)
-	scanner.Scan()
-	input := scanner.Text()
+	input := cli.Input("")
 	value := obj.Value{
 		Content: []string{},
 		Type:    fract.VALString,
