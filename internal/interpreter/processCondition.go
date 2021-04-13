754b474ad078d72f735851c81a2ffc703c6a4ce1
mertcandav <mertcandav@outlook.com>
2021-04-13 13:18:37 +0300

    Optimized comparison system.

--- internal/interpreter/processCondition.go
+++ internal/interpreter/processCondition.go
@@ -26,12 +26,11 @@ var (
 // value1 Second value of comparison.
 // operator Operator of comparison.
 func compare(value0, value1 obj.Value, operator string) bool {
-	// String comparison.
-	if !value0.Array || !value1.Array {
-		data0 := value0.Content[0]
-		data1 := value1.Content[0]
-		if (data0.Type == fract.VALString && data1.Type != fract.VALString) ||
-			(data0.Type != fract.VALString && data1.Type == fract.VALString) {
+	// compare_values Compare values by operator.
+	// data0 First data to compare.
+	// data1 Secondary data to compare.
+	compare_values := func(data0 obj.DataFrame, data1 obj.DataFrame) bool {
+		if data0.Type != data1.Type {
 			return false
 		}
 
@@ -45,100 +44,72 @@ func compare(value0, value1 obj.Value, operator string) bool {
 				return false
 			}
 		case grammar.TokenGreat: // Greater.
-			if data0.Data <= data1.Data {
+			if (data0.Type == fract.VALString && data0.Data <= data1.Data) ||
+				(data0.Type != fract.VALString &&
+					arithmetic.ToArithmetic(data0.Data) <=
+						arithmetic.ToArithmetic(data1.Data)) {
 				return false
 			}
 		case grammar.TokenLess: // Less.
-			if data0.Data >= data1.Data {
+			if (data0.Type == fract.VALString && data0.Data >= data1.Data) ||
+				(data0.Type != fract.VALString &&
+					arithmetic.ToArithmetic(data0.Data) >=
+						arithmetic.ToArithmetic(data1.Data)) {
 				return false
 			}
 		case grammar.GreaterEquals: // Greater or equals.
-			if data0.Data < data1.Data {
+			if (data0.Type == fract.VALString && data0.Data < data1.Data) ||
+				(data0.Type != fract.VALString &&
+					arithmetic.ToArithmetic(data0.Data) <
+						arithmetic.ToArithmetic(data1.Data)) {
 				return false
 			}
 		case grammar.LessEquals: // Less or equals.
-			if data0.Data > data1.Data {
+			if (data0.Type == fract.VALString && data0.Data > data1.Data) ||
+				(data0.Type != fract.VALString &&
+					arithmetic.ToArithmetic(data0.Data) >
+						arithmetic.ToArithmetic(data1.Data)) {
 				return false
 			}
 		}
+
 		return true
 	}
 
-	// Array.
+	// String comparison.
+	if !value0.Array || !value1.Array {
+		data0 := value0.Content[0]
+		data1 := value1.Content[0]
+		if (data0.Type == fract.VALString && data1.Type != fract.VALString) ||
+			(data0.Type != fract.VALString && data1.Type == fract.VALString) {
+			return false
+		}
+
+		return compare_values(data0, data1)
+	}
+
+	// Array comparison.
 	if value0.Array || value1.Array {
 		if (value0.Array && !value1.Array) ||
 			(!value0.Array && value1.Array) {
 			return false
 		}
 
-		get_sum := func(value obj.Value) float64 {
-			sum := 0.
-			for _, item := range value.Content {
-				sum += arithmetic.ToArithmetic(item.Data)
-			}
-			return sum
+		if len(value0.Content) != len(value1.Content) {
+			return operator == grammar.NotEquals
 		}
 
-		switch operator {
-		case grammar.Equals: // Equals.
-			if len(value0.Content) != len(value1.Content) {
-				return false
-			}
-		case grammar.NotEquals: // Not equals.
-			if get_sum(value0) == get_sum(value1) {
-				return false
-			}
-		case grammar.TokenGreat: // Greater.
-			if get_sum(value0) <= get_sum(value1) {
-				return false
-			}
-		case grammar.TokenLess: // Less.
-			if get_sum(value0) >= get_sum(value1) {
-				return false
-			}
-		case grammar.GreaterEquals: // Greater or equals.
-			if get_sum(value0) < get_sum(value1) {
-				return false
-			}
-		case grammar.LessEquals: // Less or equals.
-			if get_sum(value0) > get_sum(value1) {
+		for index := range value0.Content {
+			if !compare_values(value0.Content[index], value1.Content[index]) {
 				return false
 			}
 		}
+
 		return true
 	}
 
-	// Single values.
-	item0 := arithmetic.ToArithmetic(value0.Content[0].Data)
-	item1 := arithmetic.ToArithmetic(value1.Content[0].Data)
-	switch operator {
-	case grammar.Equals: // Equals.
-		if item0 != item1 {
-			return false
-		}
-	case grammar.NotEquals: // Not equals.
-		if item0 == item1 {
-			return false
-		}
-	case grammar.TokenGreat: // Greater.
-		if item0 <= item1 {
-			return false
-		}
-	case grammar.TokenLess: // Less.
-		if item0 >= item1 {
-			return false
-		}
-	case grammar.GreaterEquals: // Greater or equals.
-		if item0 < item1 {
-			return false
-		}
-	case grammar.LessEquals: // Less or equals.
-		if item0 > item1 {
-			return false
-		}
-	}
-
-	return true
+	// Single value comparison.
+	return compare_values(value0.Content[0], value1.Content[0])
 }
 
 // processCondition Process conditional expression and return result.
