ed72335ec3cfddf0c12616cdd84cb3eee59f0c52
mertcandav <mertcandav@outlook.com>
2021-04-20 18:47:33 +0300

    Fix local defines is not access in functions.

--- internal/interpreter/import.go
+++ internal/interpreter/import.go
@@ -5,33 +5,12 @@
 package interpreter
 
 import (
-	"unicode"
-
 	"github.com/fract-lang/fract/pkg/fract"
 )
 
 // Import content into destination interpeter.
 func (i *Interpreter) Import() {
 	i.ready()
-
-	varLen := len(i.variables)
-	funcLen := len(i.functions)
-
-	checkFunction := func() {
-		funcLen++
-		if !unicode.IsUpper(rune(i.functions[funcLen-1].Name[0])) {
-			funcLen--
-			i.functions = i.functions[:funcLen]
-		}
-	}
-
-	checkVariable := func() {
-		varLen++
-		if !unicode.IsUpper(rune(i.variables[varLen-1].Name[0])) {
-			varLen--
-			i.variables = i.variables[:varLen]
-		}
-	}
 
 	// Interpret all lines.
 	for i.index = 0; i.index < len(i.Tokens); i.index++ {
@@ -45,19 +24,15 @@ func (i *Interpreter) Import() {
 			tokens = tokens[1:]
 			if second.Type == fract.TypeVariable { // Variable definition.
 				i.processVariableDefinition(tokens, true)
-				checkVariable()
 			} else if second.Type == fract.TypeFunction { // Function definition.
 				i.processFunction(tokens, true)
-				checkFunction()
 			} else {
 				fract.Error(second, "Syntax error, you can protect only deletable objects!")
 			}
 		case fract.TypeVariable: // Variable definition.
 			i.processVariableDefinition(tokens, false)
-			checkVariable()
 		case fract.TypeFunction: // Function definiton.
 			i.processFunction(tokens, false)
-			checkFunction()
 		case fract.TypeImport: // Import.
 			source := Interpreter{}
 			source.processImport(tokens)
