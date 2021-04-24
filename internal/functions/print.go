1d3c56ffb2e7d318779bf8b9c0c36c1d9f9252ff
mertcandav <mertcandav@outlook.com>
2021-04-24 16:59:46 +0300

    Add value check to embed print function.

--- internal/functions/print.go
+++ internal/functions/print.go
@@ -9,6 +9,9 @@ import (
 // f Function.
 // parameters Parameters.
 func Print(f obj.Function, parameters []obj.Variable) {
+	if parameters[0].Value.Content == nil {
+		fract.Error(f.Tokens[0][0], "Invalid value!")
+	}
 	fract.PrintValue(parameters[0].Value)
 	fract.PrintValue(parameters[1].Value)
 }
