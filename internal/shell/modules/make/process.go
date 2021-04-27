3411c477a501ea6506418eff64ffc75dc652a127
mertcandav <mertcand450@hotmail.com>
2021-04-27 11:46:37 +0300

    Optimize shell package.

--- internal/shell/modules/make/process.go
+++ internal/shell/modules/make/process.go
@@ -22,10 +22,10 @@ func Process(command string) {
 	if command == "" {
 		fmt.Println("This module cannot only be used!")
 		return
-	}
-	if !strings.HasSuffix(command, fract.FractExtension) {
+	} else if !strings.HasSuffix(command, fract.FractExtension) {
 		command += fract.FractExtension
 	}
+
 	if !fs.ExistFile(command) {
 		fmt.Println("The Fract file is not exists: " + command)
 		return
