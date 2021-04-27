4463eb995fa16b9cb9bde966c49e2a2484d1cfd5
mertcandav <mertcandav@outlook.com>
2021-04-27 12:47:22 +0300

    Fix cli package.

--- pkg/cli/input.go
+++ pkg/cli/input.go
@@ -5,14 +5,18 @@
 package cli
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 // Input Returns input from command-line.
 // message Input message.
 func Input(message string) string {
 	fmt.Print(message)
-	var input string
-	fmt.Scanln(&input)
-	return input
+
+	//! Don't use fmt.Scanln
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	return scanner.Text()
 }
