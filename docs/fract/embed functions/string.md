1a408b42d24063312ca57ab036b7aac0ffa37d38
mertcandav <mertcandav@outlook.com>
2021-04-28 09:35:57 +0300

    Add type parameter to embed int function.

--- docs/fract/embed functions/string.md
+++ docs/fract/embed functions/string.md
@@ -8,15 +8,21 @@ Convert to string an object.
 protected func string(object, type="object")
 ```
 
+## Parameters
++ ``object`` <br>
+Object to parse int.
++ ``type`` <br>
+Parse type.
+
+## Flags
+### "type" parameter
++ ``object`` <br>
+Parse object to string data.
++ ``parse`` <br>
+Parse value to string.
+
 ## Examples
 ```
 string(3435)                  # {[{3435 0}] false}
 string(3435, type="parse")    # 3435
 ```
-
-## Flags
-### "type" parameter
-+ ``object`` <br>
-  Parse object format.
-+ ``parse`` <br>
-  Parse value format.
--- internal/functions/int.go
+++ internal/functions/int.go
@@ -12,12 +12,37 @@ import (
 // f Function.
 // parameters Parameters.
 func Int(f obj.Function, parameters []*obj.Variable) obj.Value {
-	return obj.Value{
-		Content: []obj.DataFrame{
-			{
-				Data: fmt.Sprint(int64(arithmetic.ToArithmetic(parameters[0].Value.Content[0].Data))),
-				Type: fract.VALInteger,
+	switch parameters[1].Value.Content[0].Data { // Cast type.
+	case "strcode":
+		data := parameters[0].Value.Content[0]
+		if data.Type != fract.VALString || data.Data == "" || len(data.Data) > 1 {
+			return obj.Value{
+				Content: []obj.DataFrame{
+					{
+						Data: "-1",
+						Type: fract.VALInteger,
+					},
+				},
+			}
+		}
+		return obj.Value{
+			Content: []obj.DataFrame{
+				{
+					Data: fmt.Sprint(parameters[0].Value.Content[0].Data[0]),
+					Type: fract.VALInteger,
+				},
 			},
-		},
+		}
+	case "object":
+		fallthrough
+	default:
+		return obj.Value{
+			Content: []obj.DataFrame{
+				{
+					Data: fmt.Sprint(int64(arithmetic.ToArithmetic(parameters[0].Value.Content[0].Data))),
+					Type: fract.VALInteger,
+				},
+			},
+		}
 	}
 }
