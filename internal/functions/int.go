92f4d4c5fd0bc8ba896280d089c1b15587ed66b3
mertcandav <mertcand450@hotmail.com>
2021-04-30 14:35:17 +0300

    Add unicode support to int-string byte conversion.

--- internal/functions/int.go
+++ internal/functions/int.go
@@ -14,25 +14,16 @@ import (
 func Int(f obj.Function, parameters []*obj.Variable) obj.Value {
 	switch parameters[1].Value.Content[0].Data { // Cast type.
 	case "strcode":
-		data := parameters[0].Value.Content[0]
-		if data.Type != fract.VALString || len(data.Data) != 1 {
-			return obj.Value{
-				Content: []obj.DataFrame{
-					{
-						Data: "-1",
-						Type: fract.VALInteger,
-					},
-				},
-			}
-		}
-		return obj.Value{
-			Content: []obj.DataFrame{
-				{
-					Data: fmt.Sprint(parameters[0].Value.Content[0].Data[0]),
+		var value obj.Value
+		for _, byt := range []byte(parameters[0].Value.Content[0].Data) {
+			value.Content = append(value.Content,
+				obj.DataFrame{
+					Data: fmt.Sprint(byt),
 					Type: fract.VALInteger,
-				},
-			},
+				})
 		}
+		value.Array = len(value.Content) > 1
+		return value
 	default: // Object.
 		return obj.Value{
 			Content: []obj.DataFrame{
